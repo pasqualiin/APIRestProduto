ca9ac2d41978661dc71c18597480dd08f51071d7
Guilherme Pasqualin <guilhermepasqualin@gmail.com>
2023-05-04 12:44:44 -0300

    Adição de dependencias Maven, configuração do banco H2, criação de tabelas no banco e adição de dados no banco

--- src/main/java/com/flipcursos/demo/controller/ProductController.java
+++ src/main/java/com/flipcursos/demo/controller/ProductController.java
@@ -3,28 +3,27 @@ package com.flipcursos.demo.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.flipcursos.demo.entities.Department;
 import com.flipcursos.demo.entities.Product;
+import com.flipcursos.demo.repositories.ProductRepository;
 
 @RestController
 @RequestMapping(value = "/products")
 
 public class ProductController {
 
+	@Autowired
+	ProductRepository productRepository;
+
 	@GetMapping
 	public List<Product> getObjects() {
-		Department department1 = new Department(1L, "Tech");
-		Department department2 = new Department(2L, "Pet");
 
-		Product product1 = new Product(1L, "Macbook Pro", 4000.0, department1);
-		Product product2 = new Product(2L, "PC Gamer", 5000.0, department1);
-		Product product3 = new Product(3L, "Pet House", 300.0, department2);
-
-		List<Product> list = Arrays.asList(product1, product2, product3);
+		List<Product> list = productRepository.findAll();
 
 		return list;
 	}
