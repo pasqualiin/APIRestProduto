0ae3dcb6c1012af63fd58bd1e18da51bbacc38f4
Guilherme Pasqualin <guilhermepasqualin@gmail.com>
2023-05-04 11:54:16 -0300

    Criação de Entidades e Controller

---
+++ src/test/java/com/flipcursos/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flipcursos.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
